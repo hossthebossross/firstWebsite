c71e9f256d8d60a6450eeee65e99f8d43b75867d
Andre Jon Branchizio <andrejbranch@gmail.com>
2013-08-08 00:04:13 -0500

    follow coding standards

--- index.php
+++ index.php
@@ -1,40 +1,44 @@
-
 <html>
-<body>
+    <body>
+        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/css/bootstrap.min.css">
 
-<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/css/bootstrap.min.css">
+        <div class="row">
+            <div class="col-lg-4">...</div>
+            <div class="col-lg-4 col-lg-offset-4">...</div>
+        </div>
+        <div class="row">
+            <div class="col-lg-3 col-lg-offset-3">3 offset 3</div>
+            <div class="col-lg-3 col-lg-offset-3">3 offset 3</div>
+        </div>
+        <div class="row">
+            <div class="col-lg-6 col-lg-offset-3">...</div>
+        </div>
 
-<div class="row">
-  <div class="col-lg-4">...</div>
-  <div class="col-lg-4 col-lg-offset-4">...</div>
-</div>
-<div class="row">
-  <div class="col-lg-3 col-lg-offset-3">3 offset 3</div>
-  <div class="col-lg-3 col-lg-offset-3">3 offset 3</div>
-</div>
-<div class="row">
-  <div class="col-lg-6 col-lg-offset-3">...</div>
-</div>
+        <div style="opacity:0.3;position:absolute;left:10px;width:1000px;height:1000px;background-color:#8AC007"></div>
 
-<div style="opacity:0.3;position:absolute;left:10px;width:1000px;height:1000px;background-color:#8AC007"></div>
+        <h1 style="font-family:verdana;color:green;font-size:60px;">Rules are for those without reason.</h1>
 
-<h1 style="font-family:verdana;color:green;font-size:60px;">Rules are for those without reason.</h1>
+        <p style="font-family:arial;color:red;font-size:40px;">
+            Rules are constructed given different circumstances other than the current.
+            <br> Reason is used to deal in real time.
+        </p>
 
-<p style="font-family:arial;color:red;font-size:40px;"> Rules are constructed given different circumstances other than the current. <br> Reason is used to deal in real time.</p>
+        <p style="font-family:comics sans;color:teal;font-size:50px;"> Yella smelly fella.
+            <img src="http://www.littleamerica-twh.net/images/Buster's%20Yellow%20Fellow.jpg" width="300" height="350">
+        </p>
 
-<p style="font-family:comics sans;color:teal;font-size:50px;"> Yella smelly fella.<img src="http://www.littleamerica-twh.net/images/Buster's%20Yellow%20Fellow.jpg" width="300" height="350"></p>
+        <div style="color:#40B3DF;">
+            Colors
+            <span style="background-color:#B4009E;color:#ffffff;">Boxes</span>
+        </div>
 
-<div style="color:#40B3DF;">Colors
-<span style="background-color:#B4009E;color:#ffffff;">Boxes</span>
-</div>
+        <p>
+            <a href="http://www.flexonline.com/"> These guys look constipadted.</a>
+            This makes me feel the opposite of tingley.
+        </p>
 
-<p>
-<a href="http://www.flexonline.com/">These guys look constipadted.</a> This makes me feel the opposite of tingley.
-</p>
-
-<?php echo "Hi Andre"; ?>
-
-</body>
+        <?php echo "Hi Andre"; ?>
+    </body>
 </html>
 
 
