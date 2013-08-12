2be8ef0a0a3002595aaee0f6985a7d825ab91402
hossthebossross <hossthebossross@gmail.com>
2013-08-12 14:54:21 -0500

    login.php wuts with line 47

--- login.php
+++ login.php
@@ -8,6 +8,44 @@
         header('Location: index.php');
         die();
     }
+
 ?>
 
+<!-- user is not logged in so show the login page -->
+<html>
+    <head>
+        <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet">
+        <link href="login.css" rel="stylesheet">
+    </head>
+    <body>
+        <h1>I'M ROSS AND I'M CRAZY</h1>
+        <form action="logincheck.php" method="post" class="form-horizontal">
+            <div class="control-group">
+                <label class="control-label" for="input-username">Username</label>
+                <div class="controls">
+                    <input name="username" type="text" id="input-username" placeholder="username">
+                </div>
+            </div>
+            <div class="control-group">
+                <label class="control-label" for="input=password">password</label>
+                <div class="control">
+                     <input name="password" type="text" id="input-password" placeholder="password">
+                </div>
+            </div>
+            <div class="control-group">
+                <div class="controls">
+                    <label class="checkbox">
+                        <input name="remember_me" type="checkbox"> Remember me
+                    </label>
+                    <button type="submit" class="btn">Sign in</button>
+                </div>
+            </div>
+        </form>
+
+         <script src="http://codeorigin.jquery.com/jquery-2.0.3.min.js"></script>
++        <script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>
+    </div>
+ </form>
+    </body>
+</html>
 
