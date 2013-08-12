0a11cf12411a41cc855efa927c15416b115a9a29
Andre Jon Branchizio <andrejbranch@gmail.com>
2013-08-11 23:34:21 -0500

    start work on login_check.php

--- login_check.php
+++ login_check.php
@@ -1,1 +1,26 @@
-<?php print_r($_POST); ?>
+<?php
+
+    $userPasswords = array(
+        'andrejbranch' => '123456',
+        'hossthebossross' => '654321',
+    );
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    if (!array_key_exists($username, $userPasswords)) {
+        // redirect to login page with error username not exists
+        var_dump('user not exist');
+    }
+
+    if (!$userPasswords[$username] === $password) {
+        // redirect to login page with error password incorrect
+        var_dump('password incorrect');
+    }
+
+    // set token
+    $_SESSION['ross_token'] = 'set';
+
+    // redirect to homepage
+
+?>
