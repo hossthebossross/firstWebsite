d24dd3cc6d2d26b39327c048efb637b0f94d24de
hossthebossross <hossthebossross@gmail.com>
2013-08-16 17:31:22 -0500

    correct logout.php

--- logout.php
+++ logout.php
@@ -1,16 +1,12 @@
 <?php
 
     session_start();
-    session_unset();
-    session_destroy();
-    $_SESSION = array();
-  #  if (count($_SESSION) == 0) {
-  #  $_SESSION=array();
-  #  session_destroy();
-    //token is unset so redirect to login.php
-        header('Location: login.php');
-        die();
-  #      }
+
+    // unset the ross_token session key
+    unset($_SESSION['ross_token']);
+
+    // redirect to login
+    header('Location: login.php');
+    die;
 ?>
- # unset($_SESSION['ross_token'], $unset );
 
